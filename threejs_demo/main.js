c1937ce17d4ea6bcf1b359f5e0eb0d0d009ec0b7
Sean <seankim912@yahoo.com>
2023-08-08 23:56:18 -0500

    rendered lighting, objects, and stars

--- threejs_demo/main.js
+++ threejs_demo/main.js
@@ -1,24 +1,73 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/*
+    Three essential components: a scene, camera, and renderer.
+*/
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene(); // Acts as a container that holds all objects, lights, etc.
+
+// Arguments for camera are: field of view in degrees, aspect ratio of user's browser window, and last two are view frustrum or what is visible relative to camera itself.
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100); // The actual 3D shape supported by 3js
+// Material is rapping paper for geometry. 3js has default materials with different properties, but there are custom shaders using WEBGL
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } ); // Standard materials need lighting to be visible.
+// const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material ); // The mesh is the actual combination of geometry and material.
+
+scene.add(torus); // Actually adds the mesh
+
+const pointLight = new THREE.PointLight(0xffffff); // Emits light in all directions.
+pointLight.position.set(0, 0, 0);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight); // Add helper wireframe to see light source.
+const gridHelper = new THREE.GridHelper(200, 50); // Adds grid to help with development.
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement); // Instantiates imported examples class that allows camera control.
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff} );
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)); // code to generate stars at random coordinates.
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const skyTexture = new THREE.TextureLoader().load('sky.jpg');
+scene.background = skyTexture;
+
+// Below is recursive function so we don't have to call the render method over and over again, similar to a game loop.
+function animate() {
+    requestAnimationFrame(animate); // Mechanism that tells the browser you want to perform an animation.
+
+    torus.rotation.x += 0.01; // Rotate horizontally this amount per frame
+    torus.rotation.y += 0.005; // Rotate vertically this amount per frame
+    torus.rotation.z += 0.01;
+
+    controls.update(); // Ensures control inputs from user, such as from OrbitControls with mouse, are detected during animation.
+
+    renderer.render(scene, camera);
+}
+
+animate();
